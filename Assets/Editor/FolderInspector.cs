60d50aaa7001d4bb04b1b93dea9fcd675e1e8847
Rodger Francis <rodger.francis@snode.com>
2020-07-23 21:21:14 +0200

    Editor folder with poc for file watcher.

---
+++ Assets/Editor/FolderInspector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+
+
+
+public class FolderInspector : AssetPostprocessor
+{
+       private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+              string[] movedFromAssetPaths)
+       {
+              Debug.Log(importedAssets[0]);
+              Debug.Log("imported");
+              
+              
+             // throw new NotImplementedException();
+       }
+}
