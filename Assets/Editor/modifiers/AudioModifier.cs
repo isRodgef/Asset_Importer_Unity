0e8365b0b39ed3694929ecf83ed11ce3df9e16f3
Rodger Francis <rodger.francis@snode.com>
2020-07-26 19:29:01 +0200

    Added interfaces for modifiers

---
+++ Assets/Editor/modifiers/AudioModifier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioModifier : AssetModifier
+{
+    public void modifyAttributes()
+    {
+     //   throw new System.NotImplementedException();
+    }
+}
